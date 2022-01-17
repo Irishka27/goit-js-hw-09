edd9adfcc435a80f2787ce3d57a5da3fd1940088
Irina Skopetskaya <sairina2704@gmail.com>
2022-01-17 17:45:23 +0200

    3-task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import Notiflix from 'notiflix';
+const form = document.querySelector('.form');
+form.addEventListener('submit', onBtnSubmit);
+let position = 0;
+function onBtnSubmit(e){
+  e.preventDefault();
+  let delay = Number(e.currentTarget.elements.delay.value);
+  const step = Number(e.currentTarget.elements.step.value);
+  const amount = Number(e.currentTarget.elements.amount.value);
+  setInterval(() => {
+    if(position == amount){return}
+    position += 1;
+    setTimeout(() => {
+      delay += step;
+    })
+    createPromise(position, delay)
+    .then(({ position, delay }) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notiflix.Notify.warning(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+  }, delay);
 }
+function createPromise(position, delay) {
+  const promise = new Promise((resolve, reject) => {
+    setInterval(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay});
+      }  
+       reject({position, delay});
+      
+    }, delay);
+  })
+  return promise;
+ }
